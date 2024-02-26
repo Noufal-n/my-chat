018420080ed9e0093b44e269eb4110e99e0e5c74
Muhammed Noufal N <muhammednoufaln369@gmail.com>
2024-02-26 21:39:48 +0530

    fixed log out and session manage

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,50 @@
 var express = require('express');
 var router = express.Router();
-const session = require('express-session')
 const passport = require('passport')
 require('../google-authintication/Oauth')
 
 
-
+function logincheck(req, res, next) {
+  req.user ? next() : res.sendStatus(403);
+}
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login');
+  if(req.user){
+    res.redirect('/chat-socket')
+  }else{
+    res.render('login');
+
+  }
 });
 
 
-router.get('/chat',(req,res)=>{
-  res.render('chat',{title:'My chat'})
+router.get('/chat',
+    passport.authenticate('google', {
+        successRedirect: '/chat-socket',
+        failureRedirect: '/failure'
+    }))
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
+
+
+router.get('/failure', (req, res) => {
+  res.send('something went wrong')
+})
+router.get('/logout',(req,res)=>{
+  req.logout(out=>{
+     
+      req.session.destroy()
+      res.send('logout')
+  })
+ 
+})
+
+router.get('/chat-socket',logincheck,(req,res)=>{
+
+  res.render('chat',{title:'My chat',img:req.user.photos[0].value})
 })
 
 
